e16c3550f7e1f2ad90807d24d0de1f461bf200f0
Gerard Crowe <gerard@Gerards-MacBook-Pro.local>
2018-07-09 21:40:24 +0100

    First commit

---
+++ ListOverview/src/webparts/ideaOverview/components/IIdeaOverviewProps.ts
@@ -0,0 +1,3 @@
+export interface IIdeaOverviewProps {
+  description: string;
+}
---
+++ ListOverview/src/webparts/ideaOverview/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIdeaOverviewWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IdeaOverviewWebPartStrings' {
+  const strings: IIdeaOverviewWebPartStrings;
+  export = strings;
+}
